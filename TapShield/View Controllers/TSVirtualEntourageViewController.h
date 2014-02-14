e5c04a82fce5289a9977f025a1ecf9675e0270a0
Ben Boyd <beathan@gmail.com>
2014-02-14 15:18:32 -0500

    Add Virtual Entourage view controller for determining destination

---
+++ TapShield/View Controllers/TSVirtualEntourageViewController.h
@@ -0,0 +1,15 @@
+//
+//  TSVirtualEntourageViewController.h
+//  TapShield
+//
+//  Created by Ben Boyd on 2/14/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSVirtualEntourageViewController : UIViewController
+
+- (IBAction)dismiss:(id)sender;
+
+@end
---
+++ TapShield/View Controllers/TSVirtualEntourageViewController.m
@@ -0,0 +1,42 @@
+//
+//  TSVirtualEntourageViewController.m
+//  TapShield
+//
+//  Created by Ben Boyd on 2/14/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import "TSVirtualEntourageViewController.h"
+
+@interface TSVirtualEntourageViewController ()
+
+@end
+
+@implementation TSVirtualEntourageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)dismiss:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end

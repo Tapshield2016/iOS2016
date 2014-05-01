ae677017845bc25393c7f331df7b2bedc7318594
Adam Share <adam@tapshield.com>
2014-05-01 08:28:47 +0800

    create and check region for entourage

---
+++ TapShield/View Controllers/TSForgotPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSForgotPasswordViewController.h
+//  TapShield
+//
+//  Created by Adam Share on 5/1/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import "TSNavigationViewController.h"
+
+@interface TSForgotPasswordViewController : TSNavigationViewController
+
+@end
---
+++ TapShield/View Controllers/TSForgotPasswordViewController.m
@@ -0,0 +1,49 @@
+//
+//  TSForgotPasswordViewController.m
+//  TapShield
+//
+//  Created by Adam Share on 5/1/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import "TSForgotPasswordViewController.h"
+
+@interface TSForgotPasswordViewController ()
+
+@end
+
+@implementation TSForgotPasswordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

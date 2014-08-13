1c1813a902f03cee9f8ee8cb9a8e26e4d1f47c1e
Adam Share <adam@tapshield.com>
2014-08-13 18:42:20 -0400

    demo for chic

---
+++ TapShield/TSUserSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  TSUserSessionManager.h
+//  TapShield
+//
+//  Created by Adam Share on 8/13/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSUserSessionManager : NSObject
+
+@end
---
+++ TapShield/TSUserSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  TSUserSessionManager.m
+//  TapShield
+//
+//  Created by Adam Share on 8/13/14.
+//  Copyright (c) 2014 TapShield, LLC. All rights reserved.
+//
+
+#import "TSUserSessionManager.h"
+
+@implementation TSUserSessionManager
+
+@end
